fc890ee6f5d4efb50c39c24368f2a0f99c4d2d71
cooler-SAI <coolerme86@gmail.com>
2025-05-10 20:33:15 +0300

    feat: Add periodic "All good" message
    This commit adds a goroutine that logs the message "All good" every 20 seconds. This goroutine is launched within the main loop, resulting in multiple instances running concurrently.

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -21,10 +23,20 @@ func main() {
 
 	log.Info().Msg("Program Started!")
 
+	signs := make(chan os.Signal, 1)
+	signal.Notify(signs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		<-signs
+		log.Info().Msg("Program Stopped!")
+		os.Exit(0)
+	}()
+
 	for {
 		log.Info().Msg("Checking...")
-
 		time.Sleep(5 * time.Second)
-
+		go func() {
+			log.Info().Msg("All good")
+			time.Sleep(20 * time.Second)
+		}()
 	}
 }
