2110340a34aa3dc99aa2c23fa15c258af350ea19
cooler-SAI <coolerme86@gmail.com>
2025-05-10 20:59:53 +0300

    feat: Add Prometheus metrics and a metrics server
    This commit introduces Prometheus metrics to track the number of "Checking..." messages logged. It also sets up a metrics server on port 2112 to expose these metrics for scraping by Prometheus. The server runs in a separate goroutine and will log a fatal error if it fails to start.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -18,10 +21,27 @@ func ZerologInit() {
 
 }
 
+var (
+	checkingMessageCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "checking_message_counter",
+			Help: "Number of messages that were sent to a checking server",
+		})
+)
+
 func main() {
 	ZerologInit()
 
+	prometheus.MustRegister(checkingMessageCounter)
+
 	log.Info().Msg("Program Started!")
+
+	go func() {
+		log.Info().Msg("Starting metrics server on port 2112")
+		http.Handle("/metrics", promhttp.Handler())
+		log.Fatal().Err(http.ListenAndServe(":2112", nil)).Msg("Metrics server failed")
+
+	}()
 
 	signs := make(chan os.Signal, 1)
 	signal.Notify(signs, syscall.SIGINT, syscall.SIGTERM)
@@ -33,6 +53,8 @@ func main() {
 
 	for {
 		log.Info().Msg("Checking...")
+		checkingMessageCounter.Inc()
+
 		time.Sleep(5 * time.Second)
 		go func() {
 			log.Info().Msg("All good")
